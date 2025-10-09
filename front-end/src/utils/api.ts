4ddf6d83e01d5a804c691d6246ccd1fda8259aa6
IsaacAlbiston <isaacalbiston@gmail.com>
2025-10-09 16:35:12 +0100

    Adding API call to fetch artworks based on search term

---
+++ front-end/src/utils/api.ts
@@ -0,0 +1,14 @@
+import axios from "axios"
+import type { searchParams } from "../models/ApiModels";
+
+const scienceMuseumGroupApi = axios.create({
+    baseURL: "https://collection.sciencemuseumgroup.org.uk",
+  });
+scienceMuseumGroupApi.defaults.headers.common['Accept'] = 'application/json'
+
+export const searchArtworks: React.FC<searchParams> = (givenParams)=>{
+    return scienceMuseumGroupApi.get("/search", {params:{...givenParams}})
+    .then(res=>{
+        return res.data.data
+    })
+}
